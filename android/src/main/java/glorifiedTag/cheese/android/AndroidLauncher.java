d634bec199bc05f4f8440d5c2f34c9ae5544e0b4
noname101o1 <owennelson101o@gmail.com>
2025-09-15 12:27:36 -0600

    first commit

---
+++ android/src/main/java/glorifiedTag/cheese/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package glorifiedTag.cheese.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import glorifiedTag.cheese.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
